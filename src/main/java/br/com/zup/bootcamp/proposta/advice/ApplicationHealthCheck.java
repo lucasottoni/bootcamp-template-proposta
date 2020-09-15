063744e5f70637f5aa457ca50556c50f74cf347a
Lucas Daniel Ottoni <lucasottoni@gmail.com>
2020-09-15 10:55:09 -0300

    adding health check

---
+++ src/main/java/br/com/zup/bootcamp/proposta/advice/ApplicationHealthCheck.java
@@ -0,0 +1,28 @@
+package br.com.zup.bootcamp.proposta.advice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationHealthCheck implements HealthIndicator {
+
+    private final Environment environment;
+
+    public ApplicationHealthCheck(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public Health health() {
+        Map<String, Object> details = new HashMap<>();
+        details.put("APP_VERSION", environment.getProperty("application.version"));
+        details.put("DESCRIPTION", "Checking application");
+
+        return Health.up().withDetails(details).build();
+    }
+}
